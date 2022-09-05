3ca9cc59f1bd91faed8578466df7adba2d48c993
Trungdp <dang.trung@marketenerprise.vn>
2022-09-05 15:00:57 +0700

    add project

---
+++ finance-next/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={"bg-gray-500"}>
+      clear
+    </div>
+  )
+}
+
+export default Home
---
+++ finance-next/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
